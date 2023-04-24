08d9523e63b4eaac7e34ff9001a2068f5e354d10
sylbaerchen <hi.silviabar@gmail.com>
2023-04-25 01:15:48 +0200

    ✨ add date

--- weather-app/src/components/TimeAndLocation.jsx
+++ weather-app/src/components/TimeAndLocation.jsx
@@ -1,16 +1,22 @@
 import React from 'react'
 
-function TimeAndLocation() {
+let d = new Date();
+let date = d.getDate();
+let year = d.getFullYear();
+let month = d.toLocaleString('default', {month: 'long'});
+let day = d.toLocaleString('default', {weekday: 'long'})
+
+
+function TimeAndLocation( ) {
   return (
     <div>
         <div className='flex items-center justify-center my-6'>
             <p className='text-white text-xl font-extralight'>
-                TIME
+                {day}, {date} {month} {year} 
             </p>
         </div>
         <div className='flex items-center justify-center my-3'>
-            <p className='text-white text-3xl font-medium'>
-                LOCATION
+            <p className='text-white text-3xl font-medium'>              
             </p>
         </div>
     </div>
