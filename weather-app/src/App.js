2e197e41f1f00cb9a11d9de528b53a3547df9715
sylbaerchen <hi.silviabar@gmail.com>
2023-04-25 01:14:50 +0200

    ✨ add new components to app

--- weather-app/src/App.js
+++ weather-app/src/App.js
@@ -1,11 +1,9 @@
-import React, {useEffect, useState} from 'react'
-import UilReact from '@iconscout/react-unicons/icons/uil-react'
-import TopButtons from './components/TopButtons'
+import React, {useState} from 'react'
 import Inputs from './components/Inputs'
 import TimeAndLocation from './components/TimeAndLocation'
 import TemperatureAndDetails from './components/TemperatureAndDetails'
 import Forecast from './components/Forecast'
-
+import Greetings from './components/Greetings'
 
 function App() {
 const apiKey = process.env.REACT_APP_APIKEY
@@ -23,23 +21,51 @@ const fetchWeather = (event) => {
   }
 }
 
-  
+let emoji = null;
+if(typeof weatherData.main != 'undefined') {
+if(weatherData.weather[0].main === 'Clear') {
+  emoji = "/sun.png" 
+} else if(weatherData.weather[0].main === 'Clouds') {
+  emoji = "/cloud.png" 
+} else if(weatherData.weather[0].main === 'Thunderstorm') {
+  emoji = "/thunder.png" 
+} else if(weatherData.weather[0].main === 'Drizzle') {
+  emoji = "/drizzle.png" 
+} else if(weatherData.weather[0].main === 'Rain') {
+  emoji = "/shower.png" 
+} else if(weatherData.weather[0].main === 'Snow') {
+  emoji = "/snow.png" 
+} else {
+  emoji = "/cloud-sun.png"
+}}
+
   return (
-    <div className='mx-auto max-w-screen-md mt-4 py-5 px-32 bg-gradient-to-br from-cyan-700 to-blue-700 h-fit shadow-xl'>
-      <TopButtons/>
-      <Inputs  
+    <div className='mx-auto max-w-screen-md pt-16 py-5 px-32 bg-gradient-to-br from-cyan-700 to-blue-700 h-fit shadow-xl'>
       
+      <Inputs     
       onchangeHandler={e => setCity(e.target.value)} 
       value={city} 
-      onkeypress={fetchWeather}/>
+      onkeypress={fetchWeather}
+     /*  handleSubmit={setCity(city)} */
+      />
       {typeof weatherData.main === 'undefined' ? (
-        <h1>Hello and welcome!</h1>  // TODO add greetings
+       <Greetings/> 
       ) : (
       <div>
        <TimeAndLocation />
-       {/* <p>{weatherData.main.temp}</p> */}
-       <TemperatureAndDetails name={weatherData.name} temperature={weatherData.main.temp} feels={weatherData.main.feels_like} humidity={weatherData.main.humidity} wind={weatherData.wind.speed} max={weatherData.main.temp_max} min={weatherData.main.temp_min}/>
+       <TemperatureAndDetails 
+       name={weatherData.name} 
+       sky={weatherData.weather[0].main}
+       wemoji={emoji}
+       temperature={Math.round(weatherData.main.temp)} 
+       feels={Math.round(weatherData.main.feels_like)} 
+       humidity={Math.round(weatherData.main.humidity)} 
+       wind={weatherData.wind.speed} 
+       max={Math.round(weatherData.main.temp_max)} 
+       min={Math.round(weatherData.main.temp_min)}/>
+       
        <Forecast/>
+       
        <Forecast/>     
      </div>) }
      {weatherData.cod === '404' && <p>Sorry, city not found :( Please try again!</p> }
@@ -49,3 +75,6 @@ const fetchWeather = (event) => {
   }
 
 export default App 
+
+/* credits */
+{/* <a href="https://www.flaticon.com/free-icons/sun" title="sun icons">Sun icons created by DinosoftLabs - Flaticon</a> */}
