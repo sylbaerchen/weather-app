db010bed8296bc6373678aa020613f5b458f655b
sylbaerchen <hi.silviabar@gmail.com>
2023-04-24 12:36:48 +0200

    💄 style bg

--- weather-app/src/App.js
+++ weather-app/src/App.js
@@ -16,7 +16,7 @@ function App() {
   }
   
   return (
-    <div >
+    <div className='mx-auto max-w-screen-md mt-4 py-5 px-32 bg-gradient-to-br from-cyan-700 to-blue-700 h-fit shadow-xl'>
       <UilReact/>
       <input 
       className='input' 
