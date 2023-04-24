b176edfb056f1645cc1bfa040bc02b7899e0f0bd
sylbaerchen <hi.silviabar@gmail.com>
2023-04-24 14:04:21 +0200

    ✨ add time and location components

---
+++ weather-app/src/components/TimeAndLocation.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+
+function TimeAndLocation() {
+  return (
+    <div>
+        <div className='flex items-center justify-center my-6'>
+            <p className='text-white text-xl font-extralight'>
+                TIME
+            </p>
+        </div>
+        <div className='flex items-center justify-center my-3'>
+            <p className='text-white text-3xl font-medium'>
+                LOCATION
+            </p>
+        </div>
+    </div>
+  )
+}
+
+export default TimeAndLocation
