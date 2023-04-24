bdb648d78f812f77343388ff544bca7b3efb2f8e
sylbaerchen <130690490+sylbaerchen@users.noreply.github.com>
2023-04-25 01:18:48 +0200

    Fetch data from api (#4)
    * ✨ pass props
    
    * ✨ add logic and states
    
    * ✨ remove import
    
    * ✨ add greetings component
    
    * 💄 style greetings
    
    * 🍱  add wemoji
    
    * ✨ add details dynamic data
    
    * ✨ add new components to app
    
    * ✨ add input props
    
    * ✨ add date

--- weather-app/src/App.js
+++ weather-app/src/App.js
@@ -1,55 +1,80 @@
 import React, {useState} from 'react'
-import UilReact from '@iconscout/react-unicons/icons/uil-react'
-import TopButtons from './components/TopButtons'
 import Inputs from './components/Inputs'
 import TimeAndLocation from './components/TimeAndLocation'
 import TemperatureAndDetails from './components/TemperatureAndDetails'
 import Forecast from './components/Forecast'
+import Greetings from './components/Greetings'
 
 function App() {
+const apiKey = process.env.REACT_APP_APIKEY
 
-  const apiKey = process.env.REACT_APP_APIKEY
-  const [weatherData, setWeatherData] = useState([{}])
-  const [city, setCity] = useState('')
+const [weatherData, setWeatherData] = useState([{}])
+const [city, setCity] = useState('')
 
-  const getWeather = (event) => {
-    if (event.key === 'Enter') {
-      fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&APPID=${apiKey}`).then(response => response.json()).then(data=> {
-        setWeatherData(data)
-      })
-    }
+
+const fetchWeather = (event) => {
+  if (event.key === 'Enter') {
+    fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&APPID=${apiKey}`).then(response => response.json()).then(data=> {
+      setWeatherData(data)
+      console.log(data)
+    })
   }
-  
-  return (
-    <div className='mx-auto max-w-screen-md mt-4 py-5 px-32 bg-gradient-to-br from-cyan-700 to-blue-700 h-fit shadow-xl'>
-      <TopButtons/>
-      <Inputs />
-      <TimeAndLocation/>
-      <TemperatureAndDetails/>
-      <Forecast/>
-      <Forecast/>
-      <input 
-      className='input' 
-      placeholder='Enter city ...' 
-      onChange={e => setCity(e.target.value)} 
-      value={city} 
-      onKeyPress={getWeather}
-      />
-    {typeof weatherData.main === 'undefined' ? (
-      <div>
-        <p className='text-2xl font-bold text-pink-500'>Welcome to Weather App! Please enter a city to get the weather</p>
-      </div>
-    ) : (
-      <div className='weather-data'>
-        <p>{weatherData.name}</p>
-        <p>{Math.round(weatherData.main.temp)} Celsius</p>
-        <p>{weatherData.weather[0].main} </p>
-        </div>
-    )}  
-    {weatherData.cod === '404' && <p>City not Found :(</p> }
-    </div>
-    )
+}
 
+let emoji = null;
+if(typeof weatherData.main != 'undefined') {
+if(weatherData.weather[0].main === 'Clear') {
+  emoji = "/sun.png" 
+} else if(weatherData.weather[0].main === 'Clouds') {
+  emoji = "/cloud.png" 
+} else if(weatherData.weather[0].main === 'Thunderstorm') {
+  emoji = "/thunder.png" 
+} else if(weatherData.weather[0].main === 'Drizzle') {
+  emoji = "/drizzle.png" 
+} else if(weatherData.weather[0].main === 'Rain') {
+  emoji = "/shower.png" 
+} else if(weatherData.weather[0].main === 'Snow') {
+  emoji = "/snow.png" 
+} else {
+  emoji = "/cloud-sun.png"
+}}
+
+  return (
+    <div className='mx-auto max-w-screen-md pt-16 py-5 px-32 bg-gradient-to-br from-cyan-700 to-blue-700 h-fit shadow-xl'>
+      
+      <Inputs     
+      onchangeHandler={e => setCity(e.target.value)} 
+      value={city} 
+      onkeypress={fetchWeather}
+     /*  handleSubmit={setCity(city)} */
+      />
+      {typeof weatherData.main === 'undefined' ? (
+       <Greetings/> 
+      ) : (
+      <div>
+       <TimeAndLocation />
+       <TemperatureAndDetails 
+       name={weatherData.name} 
+       sky={weatherData.weather[0].main}
+       wemoji={emoji}
+       temperature={Math.round(weatherData.main.temp)} 
+       feels={Math.round(weatherData.main.feels_like)} 
+       humidity={Math.round(weatherData.main.humidity)} 
+       wind={weatherData.wind.speed} 
+       max={Math.round(weatherData.main.temp_max)} 
+       min={Math.round(weatherData.main.temp_min)}/>
+       
+       <Forecast/>
+       
+       <Forecast/>     
+     </div>) }
+     {weatherData.cod === '404' && <p>Sorry, city not found :( Please try again!</p> }
+   
+    </div>
+     )
   }
 
 export default App 
+
+/* credits */
+{/* <a href="https://www.flaticon.com/free-icons/sun" title="sun icons">Sun icons created by DinosoftLabs - Flaticon</a> */}
---
+++ weather-app/src/components/Greetings.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+import { useEffect, useState } from "react";
+
+function Greetings() {
+  const [greeting, setGreeting] = useState('');
+  useEffect(() => {
+    const currentTime = new Date().getHours();
+    if (currentTime < 12) {
+      setGreeting("good morning");
+    } else if (currentTime < 18) {
+      setGreeting("good afternoon");
+    } else if (currentTime < 22) {
+      setGreeting("good evening");
+    } else {
+      setGreeting("good night");
+    }
+  }, []);
+
+  return (  
+    <div>
+      <h3 className='text-white text-lg font-medium'>Hey there, </h3>
+      <h2 className='text-white text-lg font-medium'><span className='text-teal-200 text-lg font-medium'>{greeting}</span> and welcome!</h2>
+    </div>
+  );
+}
+
+export default Greetings;
