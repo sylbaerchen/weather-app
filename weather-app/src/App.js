1552d834a393748fc97c86c9f4283e1ae1112fd8
sylbaerchen <hi.silviabar@gmail.com>
2023-04-24 14:44:45 +0200

    ✨ import components into app

--- weather-app/src/App.js
+++ weather-app/src/App.js
@@ -1,6 +1,9 @@
 import React, {useState} from 'react'
 import UilReact from '@iconscout/react-unicons/icons/uil-react'
 import TopButtons from './components/TopButtons'
+import Inputs from './components/Inputs'
+import TimeAndLocation from './components/TimeAndLocation'
+import TemperatureAndDetails from './components/TemperatureAndDetails'
 
 function App() {
 
@@ -19,6 +22,9 @@ function App() {
   return (
     <div className='mx-auto max-w-screen-md mt-4 py-5 px-32 bg-gradient-to-br from-cyan-700 to-blue-700 h-fit shadow-xl'>
       <TopButtons/>
+      <Inputs />
+      <TimeAndLocation/>
+      <TemperatureAndDetails/>
       <input 
       className='input' 
       placeholder='Enter city ...' 
